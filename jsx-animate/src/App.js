8f9b9aa20d48f8fe6ec425b8dd4d194a884b0e90
ConradMar3 <conradmare890317@gmail.com>
2022-08-27 11:05:18 +0200

    2022/08/27
    Grid correction.

--- jsx-animate/src/App.js
+++ jsx-animate/src/App.js
@@ -4,6 +4,7 @@ import HeaderExampleUsersIcon from "./components/Header";
 import {Grid, Image} from "semantic-ui-react";
 import React from "react";
 import SearchBar from "./components/SearchBar";
+import logo2 from './logo2.svg';
 
 function App() {
   return (
@@ -12,27 +13,33 @@ function App() {
 
         <HeaderExampleUsersIcon/>
 
-          <Grid centered columns={4}>
-            <Grid.Column>
-              <img src={logo} className="App-logo" alt="logo" />
-              <img src={logo} className="App-logo1" alt="logo1" />
-              <img src={logo} className="App-logo2" alt="logo2" />
-            </Grid.Column>
-          </Grid>
-
-
+        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo1" alt="logo" />
+        <img src={logo} className="App-logo2" alt="logo" />
+        <img src={logo2} className="App-logo3" alt="logo" />
+        <img src={logo2} className="App-logo4" alt="logo" />
+        <img src={logo2} className="App-logo5" alt="logo" />
+        <img src={logo} className="App-logo6" alt="logo" />
+        <img src={logo} className="App-logo7" alt="logo" />
+        <img src={logo} className="App-logo8" alt="logo" />
+        <img src={logo2} className="App-logo9" alt="logo" />
+        <img src={logo2} className="App-logo10" alt="logo" />
+        <img src={logo2} className="App-logo11" alt="logo" />
         <SearchBar></SearchBar>
-        <p>
-          But wait... There's more!
-        </p>
         <a
-          className="App-link"
-          href="https://github.com/ConradMar3"
-          target="_blank"
-          rel="noopener noreferrer"
+            className="App-link"
+            href="https://github.com/ConradMar3"
+            target="_blank"
+            rel="noopener noreferrer"
         >
           GitHub.com/ConradMar3
         </a>
+        <p>
+          Visit...
+        </p>
+
+
+
       </header>
     </div>
   );
